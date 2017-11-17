f7472953e104ab29e3410f6953ec44c298cdbf34
Dmitrijs Mihailovs <dimka62@inbox.lv>
2017-11-17 14:16:47 +0200

    Move default settings for seeding to config

--- Config/config.php
+++ Config/config.php
@@ -2,8 +2,119 @@
 
 return [
     // Cache key
-    'cache_key' => 'settings',
+    'cache_key'        => 'settings',
 
     // Upload path
-    'upload_path' => '/uploads/settings'
+    'upload_path'      => '/uploads/settings',
+
+    // Default settings to seed
+    'default_settings' => [
+        [
+            'group' => 'scripts',
+            'name'  => 'Google Analytics API key',
+            'key'   => 'google_analytics_api_key',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'scripts',
+            'name'  => 'Google Analytics client ID',
+            'key'   => 'google_analytics_client_id',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'scripts',
+            'name'  => 'Google Analytics script',
+            'key'   => 'google_analytics_script',
+            'type'  => 'textarea',
+        ],
+        [
+            'group' => 'scripts',
+            'name'  => 'Custom javascripts',
+            'key'   => 'custom_js',
+            'type'  => 'textarea',
+        ],
+        [
+            'group' => 'scripts',
+            'name'  => 'Custom styles',
+            'key'   => 'custom_css',
+            'type'  => 'textarea'
+        ],
+
+        [
+            'group' => 'seo',
+            'name'  => 'Meta keywords',
+            'key'   => 'meta_keywords',
+            'type'  => 'textarea',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'Meta description',
+            'key'   => 'meta_description',
+            'type'  => 'textarea',
+        ],
+
+        // Facebook OG tags
+        [
+            'group' => 'seo',
+            'name'  => 'OG title',
+            'key'   => 'og_title',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'OG type',
+            'key'   => 'og_type',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'OG URL',
+            'key'   => 'og_url',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'OG description',
+            'key'   => 'og_description',
+            'type'  => 'textarea'
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'OG image',
+            'key'   => 'og_image',
+            'type'  => 'text'
+        ],
+
+        // Twitter cards
+        [
+            'group' => 'seo',
+            'name'  => 'Twitter Card title',
+            'key'   => 'twitter_title',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'Twitter Card site',
+            'key'   => 'twitter_site',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'Twitter Card type',
+            'key'   => 'twitter_card',
+            'type'  => 'text',
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'Twitter Card description',
+            'key'   => 'twitter_description',
+            'type'  => 'textarea'
+        ],
+        [
+            'group' => 'seo',
+            'name'  => 'Twitter Card image',
+            'key'   => 'twitter_image',
+            'type'  => 'text'
+        ]
+    ]
 ];
